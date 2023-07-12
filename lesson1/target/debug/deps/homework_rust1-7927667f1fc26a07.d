776ef18cfcc9f68e38a863e2c152443c6643ea75
sunny <sunnydayz2000@163.com>
2023-07-12 22:32:35 +0800

    fix: gitignore

--- lesson1/target/debug/deps/homework_rust1-7927667f1fc26a07.d
+++
@@ -1,8 +0,0 @@
-/home/peter/CLionProjects/homework_rust1/target/debug/deps/homework_rust1-7927667f1fc26a07: src/main.rs src/mod_outter.rs src/mod_inner/mod.rs src/mod_inner/fun.rs
-
-/home/peter/CLionProjects/homework_rust1/target/debug/deps/homework_rust1-7927667f1fc26a07.d: src/main.rs src/mod_outter.rs src/mod_inner/mod.rs src/mod_inner/fun.rs
-
-src/main.rs:
-src/mod_outter.rs:
-src/mod_inner/mod.rs:
-src/mod_inner/fun.rs:
